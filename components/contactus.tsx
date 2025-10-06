fd0b0896e50dac6f2dcfd976f7cff838a87d2443
TinsaeMia <tinsaetesfaye2003@gmail.com>
2025-10-06 10:46:52 +0300

    changesagain2

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -5,14 +5,8 @@ import { Button } from "@/components/ui/button"; // Adjust import based on your 
 import { Input } from "@/components/ui/input";  // Adjust import based on your folder structure
 import { Textarea } from "@/components/ui/textarea"; // Adjust import based on your folder structure
 import { Card } from "@/components/ui/card"; // Import Card for the text column
-import { useSearchParams } from "next/navigation";
 
 export default function ContactForm() {
- 
-const searchParams = useSearchParams(); // Access search params
-    const v = searchParams.get('v'); // Get the value of 'v'
-    const c = searchParams.get('c'); // Get the value of 'c'
-
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="flex flex-col md:flex-row w-full max-w-7xl mx-auto space-y-4 md:space-y-0">
@@ -29,8 +23,6 @@ const searchParams = useSearchParams(); // Access search params
                         <Textarea id="message" name="message" required />
 
                         <Button type="submit" className="w-full">Send</Button>
-                         {v && <p>Email sent successfully! </p>}
-            {c && <p>Email not sent successfully Resend !</p>}
                     </form>
                 </Card>
 
@@ -40,8 +32,8 @@ const searchParams = useSearchParams(); // Access search params
                     <p className="mb-4">
                         Thank you for considering K & K Medical Transport for your non-emergency medical transportation needs. We are here to assist you and look forward to answering any questions you may have or helping you schedule a transportation service.
                     </p>
-                    <p className="mb-2"><span className="mr-2">📧</span>kkmedicaltransport@gmail.com</p>
-                    <p className="mb-2">  <span className="mr-2">📞</span> 512 945-4047</p>
+                    <p className="mb-2"><strong>Email:</strong> kkmedicaltransport@gmail.com</p>
+                    <p className="mb-2"><strong>Phone:</strong> 512 945-4047</p>
                     <h3 className="font-bold mt-4">Business Hours</h3>
                     <ul className="list-disc list-inside mt-2">
                         <li>Monday: 7 AM–7 PM</li>
